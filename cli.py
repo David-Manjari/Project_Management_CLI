c770bc9db25fbf81a92a3c18fdea241c77769bf3
Manjari <manjari@Manjari.localdomain>
2026-07-20 07:30:16 +0300

    created methods to validate user input and add the users,projetcs and tasks

--- cli.py
+++ cli.py
@@ -2,3 +2,31 @@ import argparse
 from .models import User,Project,Task
 
 users = {}
+
+def add_user(args):
+    if args.name in users:
+        print("User already exists")
+        return
+    users[args.name] = User(args.name)
+    print(f"User {args.name} added")
+
+def add_project(args):
+    user = users.get(args.user)
+    if not user:
+        print("User not found")
+        return
+    project = Project(args.title)
+    user.add_project(project)
+
+def add_task(args):
+    user = users.get(args.user)
+    if not user:
+        print("User not found")
+        return
+    project = user.get_project(args.project)
+
+    if not project:
+        print("Project not found")
+        return
+    task = Task(args.title)
+    project.add_task(task)
